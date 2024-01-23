5a1a5a66ca40d508914176feb6773f55c4aa14f3
saleh sheikh <salehsheikh786@gmail.com>
2024-01-24 03:42:03 +0500

    modifying app and main with provider

--- e-store/src/App.jsx
+++ e-store/src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Carousel from './components/Carousel'
-import HeroSection from './components/HeroSection'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import HeroSection from './components/HeroSection';
+import SingleProduct from './components/SingleProduct';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Carousel/>
-      <HeroSection/>
-    </div>
-  )
-}
+    <Router>
+      <Navbar />
+      <Carousel />
+      <HeroSection />
+      <Routes>
+        <Route path="/singleProduct/:id" element={<SingleProduct />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+};
 
-export default App
+export default App;
