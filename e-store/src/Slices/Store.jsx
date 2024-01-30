f4167c41a55b8ad95a239dcb907b86bd08d2c18f
saleh sheikh <salehsheikh786@gmail.com>
2024-01-31 04:49:27 +0500

    updating store

--- e-store/src/Slices/Store.jsx
+++ e-store/src/Slices/Store.jsx
@@ -1,10 +1,12 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './productSlice';
+import cartReducer from './cartSlice';
 
 const Store = configureStore({
   reducer: {
     products: productReducer,
+    cart: cartReducer,
   },
 });
 
