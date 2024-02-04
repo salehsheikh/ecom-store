8ed07af3486032d8a7605f5cbd90863601985ab4
saleh sheikh <salehsheikh786@gmail.com>
2024-02-05 02:31:44 +0500

    adding authentication for user

---
+++ e-store/src/auth/Login.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './Firebase';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate('/home');
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });
+  };
+
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <section className="bg-white p-8 shadow-md rounded-md w-96">
+        <h1 className="text-3xl font-bold mb-6 text-center">Login</h1>
+
+        <form className="space-y-4">
+          <div>
+            <label htmlFor="email-address" className="block text-sm font-medium text-gray-600">
+              Email address
+            </label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              required
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-600">
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              onClick={onLogin}
+              className="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-4 text-center text-sm text-gray-600">
+          No account yet?{' '}
+          <NavLink to="/signup" className="text-indigo-500 hover:underline">
+            Sign up
+          </NavLink>
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default Login;
---
+++ e-store/src/auth/SignUp.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './Firebase';
+
+const SignUp = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log(user);
+      navigate('/login');
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorCode, errorMessage);
+    }
+  };
+
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <section className="bg-white p-8 shadow-md rounded-md w-96">
+        <h1 className="text-3xl font-bold mb-6 text-center">Sign Up</h1>
+
+        <form onSubmit={onSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="email-address" className="block text-sm font-medium text-gray-600">
+              Email address
+            </label>
+            <input
+              type="email"
+              label="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Email address"
+              className="w-full p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              label="Create password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Password"
+              className="w-full p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-300"
+          >
+            Sign up
+          </button>
+        </form>
+
+        <p className="mt-4 text-center text-sm text-gray-600">
+          Already have an account?{' '}
+          <NavLink to="/login" className="text-indigo-500 hover:underline">
+            Sign in
+          </NavLink>
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default SignUp;
