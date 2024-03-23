36a21cb43516bf114643d65a64abe317fa6ccf5f
saleh sheikh <salehsheikh786@gmail.com>
2024-03-24 02:05:58 +0500

    adding status for payment

---
+++ e-store/src/components/Cancel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cancel = () => {
+  return (
+    <div className="bg-red-500 text-white p-4 rounded-md flex justify-center items-center">
+      Cancel
+    </div>
+  );
+};
+
+export default Cancel;
+
