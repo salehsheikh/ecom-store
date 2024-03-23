edf886511cafb255393eca4d12ab36e78fa9abef
saleh sheikh <salehsheikh786@gmail.com>
2024-03-24 02:06:29 +0500

    adding status for payment

---
+++ e-store/src/components/Sucess.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sucess = () => {
+  return (
+    <div className="bg-green-500 text-white p-4 rounded-md flex justify-center items-center">
+     Success
+    </div>
+  )
+}
+
+export default Sucess;
