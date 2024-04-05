ebeaccd2cc6d8123b47888ea9a25c9b011da4b8d
saleh sheikh <salehsheikh786@gmail.com>
2024-04-06 02:38:47 +0500

    production ready

--- server/app.js
+++ server/app.js
@@ -1,12 +1,27 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import path from 'path';
 const app = express();
 import cors from 'cors';
 import Stripe from 'stripe';
 
 app.use(express.json());
 app.use(cors());
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../e-store/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "../e-store/dist/index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 const stripe = new Stripe(process.env.STRIPE_SECRET);
 // checkout api
 app.post("/api/create-checkout-session",async(req,res)=>{
