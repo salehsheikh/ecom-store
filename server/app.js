642fa3395c52792dcd3d0f9a4e4c7e27b39e8ba3
salehsheikh <79030299+salehsheikh@users.noreply.github.com>
2024-04-06 20:58:27 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,10 +12,10 @@ app.use(cors());
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../e-store/dist")));
+  app.use(express.static(path.join(__dirname1, "/e-store/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "../e-store/dist/index.html"))
+    res.sendFile(path.resolve(__dirname1, "/e-store/dist/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
