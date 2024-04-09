0834439567c275e9a5da88f1ef51f4ed0891bf43
saleh sheikh <salehsheikh786@gmail.com>
2024-04-09 21:23:25 +0500

    updated

--- server/e-store/src/components/Navbar.jsx
+++ server/e-store/src/components/Navbar.jsx
@@ -7,8 +7,10 @@ import useAuth from "../auth/useAuth";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+  const cartItems = useSelector((state) => state.cart.items);
+
   const { user } = useAuth();
+  
 
   const navLinks = [
     { to: "/", label: "Home" },
@@ -32,9 +34,9 @@ const Navbar = () => {
               d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
             />
           </svg>
-          {totalQuantity > 0 && (
+          {cartItems.length > 0 && (
             <span className="relative -top-8 -right-3 bg-[#222] text-white rounded-full w-4 h-4 flex items-center justify-center">
-              {totalQuantity}
+              {cartItems.length}
             </span>
           )}
         </>
