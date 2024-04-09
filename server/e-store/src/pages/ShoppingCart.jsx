ad323820c9e25df3e854b723b948c605f19e723b
saleh sheikh <salehsheikh786@gmail.com>
2024-04-09 20:47:17 +0500

    Commit message describing your changes

--- server/e-store/src/pages/ShoppingCart.jsx
+++ server/e-store/src/pages/ShoppingCart.jsx
@@ -53,7 +53,7 @@ const ShoppingCart = () => {
     };
   
     try {
-      const response = await fetch("http://localhost:3000/api/create-checkout-session", {
+      const response = await fetch("https://server-eta-ten-46.vercel.app/api/create-checkout-session", {
         method: "POST",
         headers: headers,
         body: JSON.stringify(body)
